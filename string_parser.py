83c86b4737f616c7e57def0b949790b2ff54c65f
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2023-01-08 02:58:33 +0000

    work

--- string_parser.py
+++ string_parser.py
@@ -12,21 +12,37 @@ import random
 # import lists
 
 
-def string_parser(grade_num_list):
-    # initializing sum
-    sum = 0
-    for a_single_number in grade_num_list:
-        # reassigning sum
-        sum = sum + a_single_number
-    # initializing num_amount
-    num_amount = len(grade_num_list)
-    # checking for errors
-    if num_amount == 0:
-        return -1
-    # returning as normal
-    else:
-        final_sum = sum / num_amount
-        return final_sum
+def string_parser(user_input):
+    # initializing list
+    a_string_list = []
+    # initializing word
+    word = ""
+    # initializing length_of_string
+    length_of_string = len(user_input)
+    # initializing counter
+    counter = 0
+    # starting loop
+    
+    while counter < length_of_string:
+        print(user_input[counter])
+        current_char = user_input[counter]
+        # checking if word is over
+        if current_char == " ":
+            # adding word to list
+            a_string_list.append(word)
+            # reassigning word
+            word = ""
+        elif counter == length_of_string - 1:
+            # adding to word
+            word = word + current_char
+            # adding word to list
+            a_string_list.append(word)
+        else:
+            # adding to word
+            word = word + current_char
+        # incrementing counter
+        counter = counter + 1
+    return a_string_list
 
 
 def main():
@@ -37,40 +53,14 @@ def main():
     print("individual words separately")
     print("")
 
-    # initializing list
-    grade_num_list = []
-
     # Getting user_input
     user_input = input("Enter a sentence: ")
 
-    # generating numbers
-    while True:
-        # initializing current_char
-        current_char = user_input
-        # checking to see if loop continues
-        if user_input == "stop":
-            break
-        # checking valid input
-        # what valid input is there to check???
-
-        # checking if input is within range
-        if user_input_int > 100 or user_input_int < 0:
-            print("Please enter a grade between 1 and 100.")
-            continue
-        else:
-            # adding input to list
-            grade_num_list.append(user_input_int)
-
     # calling function
-    true_sum = string_parser(grade_num_list)
+    split_sentence = string_parser(user_input)
 
-    # checking results
-    if true_sum == -1:
-        # error message (not enough grades)
-        print("Please enter two or more grades.")
-    else:
-        # displaying results
-        print("The average grade is {}%.".format(true_sum))
+    # displaying results
+    print(split_sentence)
 
 
 if __name__ == "__main__":
